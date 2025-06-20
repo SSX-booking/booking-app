947b9310aee6ba445ff603aa7fa339220cac482a
ssx_corp <mr.dimov77@gmail.com>
2025-06-20 14:19:28 +0300

    Redundant access modifiers removed

--- src/main/java/mate/academy/bookingapp/service/booking/BookingService.java
+++ src/main/java/mate/academy/bookingapp/service/booking/BookingService.java
@@ -7,18 +7,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface BookingService {
-    public BookingResponseDto save(CreateBookingRequestDto booking, Long userId);
+    BookingResponseDto save(CreateBookingRequestDto booking, Long userId);
 
-    public Page<BookingResponseDto> findByUserIdAndStatus(
+    Page<BookingResponseDto> findByUserIdAndStatus(
             Long userId,
             BookingStatus status,
             Pageable pageable);
 
-    public Page<BookingResponseDto> findUserBookings(Long userId, Pageable pageable);
+    Page<BookingResponseDto> findUserBookings(Long userId, Pageable pageable);
 
-    public BookingResponseDto findById(Long bookingId);
+    BookingResponseDto findById(Long bookingId);
 
-    public BookingResponseDto updateBooking(
+    BookingResponseDto updateBooking(
             Long bookingId,
             CreateBookingRequestDto updatedBooking,
             Long userId);
