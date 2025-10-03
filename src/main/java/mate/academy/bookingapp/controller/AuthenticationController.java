a80b1ebc7cfe4551aed67302200ea2ef06b32060
ssx_corp <mr.dimov77@gmail.com>
2025-10-03 15:55:38 +0300

    Auth fixed with email verification

--- src/main/java/mate/academy/bookingapp/controller/AuthenticationController.java
+++ src/main/java/mate/academy/bookingapp/controller/AuthenticationController.java
@@ -4,16 +4,21 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import mate.academy.bookingapp.dto.email.EmailVerificationRequestDto;
 import mate.academy.bookingapp.dto.user.UserLoginRequestDto;
 import mate.academy.bookingapp.dto.user.UserLoginResponseDto;
 import mate.academy.bookingapp.dto.user.UserRegistrationRequestDto;
 import mate.academy.bookingapp.dto.user.UserResponseDto;
 import mate.academy.bookingapp.exception.RegistrationException;
 import mate.academy.bookingapp.security.AuthenticationService;
+import mate.academy.bookingapp.service.emailverification.EmailVerificationService;
 import mate.academy.bookingapp.service.user.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,8 +29,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     private final UserService userService;
     private final AuthenticationService authenticationService;
+    private final EmailVerificationService emailVerificationService;
 
     @PostMapping("/registration")
+    @ResponseStatus(HttpStatus.CREATED)
     @Operation(summary = "Register new user",
             description = "Register new user and returns it")
     public UserResponseDto register(@Valid @RequestBody UserRegistrationRequestDto request)
@@ -34,9 +41,26 @@ public class AuthenticationController {
     }
 
     @PostMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Login user",
             description = "Login for already existing user")
     public UserLoginResponseDto login(@Valid @RequestBody UserLoginRequestDto request) {
         return authenticationService.authenticate(request);
+    }
+
+    @PostMapping("/verify-email")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Verify email",
+            description = "Verify email for current user")
+    public String verifyEmail(@Valid @RequestBody EmailVerificationRequestDto dto) {
+        return userService.verifyCode(dto.getEmail(), dto.getCode());
+    }
+
+    @PostMapping("/resend-code")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Resend email code",
+            description = "Resend email verification code")
+    public String resendCode(@RequestParam String email) {
+        return emailVerificationService.resendVerificationCode(email);
     }
 }
--- src/main/java/mate/academy/bookingapp/security/JwtAuthenticationFilter.java
+++ src/main/java/mate/academy/bookingapp/security/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -25,9 +26,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
         String token = getToken(request);
 
